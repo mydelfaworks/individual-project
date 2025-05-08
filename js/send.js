a98e5d1f3c82cfe675a89fbeaa35d16d3aafc383
Nikita Belov <nikita2010belov@gmail.com>
2025-05-08 16:39:37 +0500

    форма

---
+++ js/send.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var button = document.getElementById("send_message");
+
+    button.addEventListener("click", (event) => {
+        alert("пiпiдастр");
+    })
+});
